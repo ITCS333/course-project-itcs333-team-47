82e24759547843b089143609e8d3c75fa93845b6
REEM SALEH <reemalmuresi200@gmail.com>
2025-12-14 22:37:00 +0300

    FIX TASK4301 AND TASK4315
    Initialize user session with default values if not set.

--- src/assignments/api/index.php
+++ src/assignments/api/index.php
@@ -4,6 +4,14 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
+
+if (!isset($_SESSION['user'])) {
+    $_SESSION['user'] = [
+        'id'   => null,
+        'role' => 'guest'
+    ];
+}
+
 // If request is for a static file (HTML / JS / CSS), serve it directly
 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $fullPath = __DIR__ . $path;
